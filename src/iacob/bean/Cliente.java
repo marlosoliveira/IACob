76e02eab2e849dd634f9ae48132fd3a60ceb7edc
Marlos Oliveira <marlosoliveira@protonmail.com>
2018-12-12 20:20:16 -0300

    c2 - criando algumas beans cliente e produto

---
+++ src/iacob/bean/Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iacob.bean;
+
+/**
+ *
+ * @author Marlos Oliveira
+ */
+public class Cliente {
+    private String cliNome;
+    private String cliEnd;
+    private String cliCNPJ;
+    private String cliCPF;
+    private String cliTel;
+    private String cliCel;
+    private String cliObs;
+}
